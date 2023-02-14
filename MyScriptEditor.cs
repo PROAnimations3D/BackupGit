e7e779cb7cafb3696078935ef770ba6db819b615
PROAnimations3D <83284762+PROAnimations3D@users.noreply.github.com>
2023-02-14 04:30:21 +0100

    Update MyScriptEditor.cs

--- MyScriptEditor.cs
+++ MyScriptEditor.cs
@@ -11,53 +11,16 @@ public class MyScriptEditor : Editor
         MyScript script = target as MyScript;
 
         EditorGUILayout.LabelField("Mass");
-        if (GUILayout.Button("Increase by 1"))
-        {
-            script.SetMass(script.GetComponent<Rigidbody>().mass + 1f);
-        }
-        if (GUILayout.Button("Decrease by 1"))
-        {
-            script.SetMass(script.GetComponent<Rigidbody>().mass - 1f);
-        }
+		EditorGUILayout.BeginHorizontal();
+		if (GUILayout.Button("Increase by 1"))
+		{
+			script.SetMass(script.GetComponent<Rigidbody>().mass + 1f);
+		}
+		if (GUILayout.Button("Decrease by 1"))
+		{
+			script.SetMass(script.GetComponent<Rigidbody>().mass - 1f);
+		}
+		EditorGUILayout.EndHorizontal();
 
-        EditorGUILayout.LabelField("Drag");
-        if (GUILayout.Button("Increase by 1"))
-        {
-            script.SetDrag(script.GetComponent<Rigidbody>().drag + 1f);
-        }
-        if (GUILayout.Button("Decrease by 1"))
-        {
-            script.SetDrag(script.GetComponent<Rigidbody>().drag - 1f);
-        }
-
-        EditorGUILayout.LabelField("Angular Drag");
-        if (GUILayout.Button("Increase by 0.05"))
-        {
-            script.SetAngularDrag(script.GetComponent<Rigidbody>().angularDrag + 0.05f);
-        }
-        if (GUILayout.Button("Decrease by 0.05"))
-        {
-            script.SetAngularDrag(script.GetComponent<Rigidbody>().angularDrag - 0.05f);
-        }
-
-        EditorGUILayout.LabelField("Use Gravity");
-        if (GUILayout.Button("True"))
-        {
-            script.SetUseGravity(true);
-        }
-        if (GUILayout.Button("False"))
-        {
-            script.SetUseGravity(false);
-        }
-
-        EditorGUILayout.LabelField("Is Kinematic");
-        if (GUILayout.Button("True"))
-        {
-            script.SetIsKinematic(true);
-        }
-        if (GUILayout.Button("False"))
-        {
-            script.SetIsKinematic(false);
-        }
     }
 }
